5013e79f771d9b5917af1e4207c5d3ef47d25b7a
Ashutosh Chapagain <ashutoshchapagain0@gmail.com>
2021-06-29 17:42:06 +0545

    comment model added

--- healthpoint/admin.py
+++ healthpoint/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 admin.site.register(Post)
+admin.site.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'content', 'post', 'date_commented')
+    list_filter = ('date_commented')
+    search_fields = ('author', 'content')
--- healthpoint/models.py
+++ healthpoint/models.py
@@ -8,6 +8,20 @@ class Post(models.Model):
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+	class Meta:
+		ordering = ['-date_posted']
+
 	def __str__(self):
 		return self.title
-	
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	content = models.TextField()
+	date_commented = models.DateTimeField(default=timezone.now)
+
+	class Meta:
+		ordering = ['-date_commented']
+
+	def __str__(self):
+		return 'Comment {} by {}'.format(self.content, self.author)
